7fc14f7b09ea6fb0c669eb3f43ec5a508030f56e
Andrii <sparrow.andriy@gmail.com>
2022-11-02 17:57:26 +0200

    calculating time spent each day

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+
 import requests
 import json
 from functools import reduce
@@ -23,7 +25,22 @@ with open('res.json', 'w', encoding='utf-8') as f:
     json.dump(result, f, indent=2, ensure_ascii=False)
 
 grouped = {}
+days = set()
 for el in result:
+    # Transform datetime strings into datetime objects
+    # This will help perform some actions
+    datetime_start = el['timeInterval']['start'].replace('T', ' ').replace('Z', '')
+    el['timeInterval']['start'] = datetime.strptime(datetime_start, '%Y-%m-%d %H:%M:%S')
+    if el['timeInterval']['end']:
+        datetime_end = el['timeInterval']['end'].replace('T', ' ').replace('Z', '')
+        el['timeInterval']['end'] = datetime.strptime(datetime_end, '%Y-%m-%d %H:%M:%S')
+
+    # Find all dates among entries
+    days.add(el['timeInterval']['start'].date())
+    if el['timeInterval']['end']:
+        days.add(el['timeInterval']['end'].date())
+
+    # Group entries by taskId
     try:
         grouped[el['taskId']].append({'description': el['description'], 'timeInterval': el['timeInterval']})
     except KeyError:
@@ -31,6 +48,37 @@ for el in result:
         grouped[el['taskId']].append({'description': el['description'], 'timeInterval': el['timeInterval']})
 
 # Time spent on each task
+print('Time spent on each task: ')
 for key, value in grouped.items():
-    print(f'id: {key} '
-          f'total time: {reduce(lambda x, y: x + y, [TimeParse(el["timeInterval"]["duration"]) for el in value])}')
+    print(f'taskId: {key}\n'
+          f'description: {value[0]["description"]}\n'
+          f'total time: {reduce(lambda x, y: x + y, [TimeParse(el["timeInterval"]["duration"]) for el in value])}\n')
+
+print('----------------------------\n')
+print('Time spent each day: ')
+
+# Time spent each day
+time_each_day = dict([(el, timedelta(0)) for el in days])
+for day in days:
+    for el in result:
+        st = el['timeInterval']['start']
+        if not (end := el['timeInterval']['end']):
+            end = datetime.strptime(datetime.now().isoformat(' ', 'seconds'), '%Y-%m-%d %H:%M:%S')
+
+        if st.date() == day:
+            if end.date() == day:
+                duration = TimeParse(el['timeInterval']['duration'])
+                time_each_day[day] += timedelta(hours=duration.h, minutes=duration.m, seconds=duration.s)
+            else:
+                tmp = day + timedelta(days=1)
+                time_each_day[day] += datetime(tmp.year, tmp.month, tmp.day, 0, 0, 0) - st
+        elif end.date() == day:
+            time_each_day[day] += end - datetime(day.year, day.month, day.day, 0, 0, 0)
+
+# Prettify and sort by date
+time_each_day = time_each_day.items()
+time_each_day = sorted(time_each_day, key=lambda x: x[0])
+time_each_day = list(map(lambda x: (str(x[0]), str(x[1])), time_each_day))
+
+for key, value in time_each_day:
+    print(f'{key} - {value}')
