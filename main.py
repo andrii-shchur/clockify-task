f80b1cf3bdd7b0762f39dd21e94d596add72f208
Andrii <sparrow.andriy@gmail.com>
2022-11-02 16:29:08 +0200

    time parser, calculated time spent on each task

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import requests
 import json
-from utils import get_user_id, get_workspace_ids, get_api_key
+from functools import reduce
 
+from utils import get_user_id, get_workspace_ids, get_api_key, TimeParse
+
+
+# Get metadata
 api_key = get_api_key()
 workspace_ids = get_workspace_ids()
 user_id = get_user_id()
 
+# Get all entries using API
 headers = {'x-api-key': api_key}
 result = []
 for workspace_id in workspace_ids:
@@ -13,5 +18,19 @@ for workspace_id in workspace_ids:
                      headers=headers)
     result.extend(json.loads(r.content))
 
-with open('res.json', 'w') as f:
-    json.dump(result, f, indent=4)
+# Dump all entries into res.json
+with open('res.json', 'w', encoding='utf-8') as f:
+    json.dump(result, f, indent=2, ensure_ascii=False)
+
+grouped = {}
+for el in result:
+    try:
+        grouped[el['taskId']].append({'description': el['description'], 'timeInterval': el['timeInterval']})
+    except KeyError:
+        grouped[el['taskId']] = []
+        grouped[el['taskId']].append({'description': el['description'], 'timeInterval': el['timeInterval']})
+
+# Time spent on each task
+for key, value in grouped.items():
+    print(f'id: {key} '
+          f'total time: {reduce(lambda x, y: x + y, [TimeParse(el["timeInterval"]["duration"]) for el in value])}')
--- utils.py
+++ utils.py
@@ -3,21 +3,56 @@ import json
 import requests
 
 
-def get_api_key():
+# Get API key specified in config.ini
+def get_api_key() -> str:
     config = configparser.ConfigParser()
     config.read('config.ini')
     return config['auth']['api_key']
 
 
-def get_workspace_ids():
+# Get all workspace ids using API
+def get_workspace_ids() -> list:
     r = requests.get(f'https://api.clockify.me/api/v1/workspaces/',
                      headers={'x-api-key': get_api_key()})
     d = json.loads(r.content)
     return [el['id'] for el in d]
 
 
-def get_user_id():
+# Get current user id using API
+def get_user_id() -> str:
     r = requests.get(f'https://api.clockify.me/api/v1/user/',
                      headers={'x-api-key': get_api_key()})
     d = json.loads(r.content)
     return d['id']
+
+
+# Class for time parser
+class TimeParse:
+    def __init__(self, time: str | None):
+        self.s = self.m = self.h = 0
+        if not time:
+            return
+
+        curr = ''
+        for el in time.strip('PT'):
+            if el == 'H':
+                self.h = int(curr)
+                curr = ''
+            elif el == 'M':
+                self.m = int(curr)
+                curr = ''
+            elif el == 'S':
+                self.s = int(curr)
+                curr = ''
+            else:
+                curr += el
+
+    def __str__(self):
+        return f'{self.h} hours, {self.m} minutes, {self.s} seconds'
+
+    def __add__(self, other):
+        s = (self.s + other.s) % 60
+        m = (self.m + other.m + (self.s + other.s) // 60) % 60
+        h = self.h + other.h + (self.m + other.m + (self.s + other.s) // 60) // 60
+
+        return TimeParse(f'PT{h}H{m}M{s}S')
